44b2d00e95e708d84050d68ee366ea78e52fd8fe
MarcoSants <marcochel@gmail.com>
2018-06-19 09:56:05 -0300

    Middlewares: authentication (finished), user (half)

---
+++ middleware/authenticationRequired.js
@@ -0,0 +1,9 @@
+function isLoged(req, res, next) {
+    if (req.session.userId && req.session) {
+      return next();
+    } else {
+      let err = new Error('Você necessita estar logado para acessar esta pagina.');
+      err.status = 401;
+      return next(err);
+    }
+}
---
+++ middleware/userVerified.js
@@ -0,0 +1,9 @@
+function isUser(req, res, next) {
+    if (1) { //TO-DO ****
+      return next();
+    } else {
+      let err = new Error('Você necessita ser o dono da pagina para editar.');
+      err.status = 401;
+      return next(err);
+    }
+}
